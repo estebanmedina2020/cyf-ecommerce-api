62b59797091bcb93859e3c86ee9278d2952a6bda
Esteban <emedina@creatonico.com>
2020-08-06 21:55:31 +0200

    updated code

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/products", function (req, res) {
     }
   );
 });
-*/
+
 app.get("/products", function (req, res) {
   let name = req.query.name;
   pool
@@ -34,6 +34,75 @@ app.get("/products", function (req, res) {
       res.json(result.rows);
     })
     .catch((e) => console.error(e));
+});
+
+app.get("/customers/:customerId", function (req, res) {
+  let customerId = req.params.customerId;
+  pool
+    .query("SELECT * FROM customers c WHERE c.id = $1", [customerId])
+    .then((result) => {
+      if (result.rowCount > 0) {
+        return res.json(result.rows[0]);
+      } else {
+        return res
+          .status(404)
+          .send(`Customer with Id = ${customerId} NOT FOUND`);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("something went wrong :( ...");
+    });
+});
+
+app.post("/customers", function (req, res) {
+  const { name, address, city, country } = req.body;
+
+  pool
+    .query(
+      "INSERT INTO customers (name, address, city, country) VALUES ($1, $2, $3, $4);",
+      [name, address, city, country]
+    )
+    .then((result) => res.status(201).send("customer created :) !"))
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("something went wrong :( ...");
+    });
+});
+*/
+
+app.post("/products", function (req, res) {
+  const { product_name, unit_price, supplier_id } = req.body;
+
+  if (unit_price < 0) {
+    console.log("integer validation");
+    return res.status(500).send("Please check the unit price :( ...");
+  }
+
+  pool
+    .query("SELECT * FROM suppliers WHERE suppliers.id = $1", [supplier_id])
+    .then((result) => {
+      if (result.rows.length < 1) {
+        return res.status(500).send("Supplier ID not found :( ...");
+      } else {
+        pool
+          .query(
+            "INSERT INTO products (product_name, unit_price, supplier_id) VALUES ($1, $2, $3);",
+            [product_name, unit_price, supplier_id]
+          )
+          .then((result) => res.status(201).send("product created :) !"))
+          .catch((error) => {
+            console.log(error);
+            res.status(500).send("oopps, something went wrong :( ...");
+          });
+      }
+    })
+    .catch((error) =>
+      res.status(500).send({
+        message: "oopps, something went wrong :( ...",
+        error,
+      })
+    );
 });
 
 app.listen(3100, function () {
